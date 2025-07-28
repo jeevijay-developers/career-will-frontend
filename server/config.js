aaed96c30724b4407b525c1b320647844ccb15be
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-07-28 15:43:42 +0530

    Remove withCredentials option from axios instance in config.js

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default apiClient;
